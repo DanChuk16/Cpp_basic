89e7a6a14e7515de4d76c31a19fb836499ccac32
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 21:01:59 +0200

    hm 2 ex 1

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    string name = "The war and the peace";
+    string author = "L.N. Tolstoj";
+    string publisher = "Piter";
+    int pages = 500;
+
+    cout << "Name       : \"" << name << "\"" << endl;
+    cout << "Avtor      : " << author << endl;
+    cout << "Izdatelstvo: " << publisher << endl;
+    cout << "Pages      : " << pages << "." << endl;
+
+    return 0;
+}
