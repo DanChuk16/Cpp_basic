b5adc3fc17788c7ac544fd919a2df86c651ac157
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 21:48:09 +0200

    hm 2 ex 4

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    double scale, distance_on_map, distance;
+    cout << "Обчислення відстані між населеними пунктами.\n";
+    cout << "Введіть вихідні дані:\n";
+    cout << "Масштаб карти (кількість кілометрів в одному сантиметрі) -> ";
+    cin >> scale;
+    cout << "Відстань між точками, які зображують населені пункти (см) -> ";
+    cin >> distance_on_map;
+    distance = distance_on_map * scale;
+    cout << "Відстань між населеними пунктами " << distance << " км.\n";
+    return 0;
+}
