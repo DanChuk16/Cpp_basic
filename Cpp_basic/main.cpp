9d522fc75841550155399ae268b7b2785a42a7c4
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 21:50:58 +0200

    hm 2 ex 5

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+  float pi = 3.14;
+  float r, V;
+  
+  cout << "Введіть радіус кулі: ";
+  cin >> r;
+  
+  V = (4.0/3.0) * pi * r * r * r;
+  
+  cout << "Об'єм кулі з радіусом " << r << " дорівнює " << V << endl;
+  
+  return 0;
+}
