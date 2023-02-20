ddc965ed0c3c5963e298b9be1036ddbba77f95a1
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 17:14:53 +0200

    hm 1 ex 1

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
 
+int main()
+{
+    std::cout << "\"Maybe won’t you take it back\nSay you were tryna make me laugh\nAnd nothing has to change today\nYou didn’t mean to say «I love you»\nI love you and I don’t want to\nOh-oh-oh\"" << std::endl;
+    std::cout << "\t\t\t\t\t\'Billie Eilish\'" << std::endl;
+    return 0;
+}
