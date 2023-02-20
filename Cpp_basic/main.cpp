1216713f66808360bba4978c1848a7014836b23c
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 21:34:04 +0200

    hm 2 ex 3

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float length, width, height, volume;
+    cout << "Обчислення об'єму паралелепіпеда.\n";
+    cout << "Введіть вихідні дані:\n";
+    cout << "Довжина (см) -> ";
+    cin >> length;
+    cout << "Ширина (см) -> ";
+    cin >> width;
+    cout << "Висота (см) -> ";
+    cin >> height;
+    volume = length * width * height;
+    cout << "Об'єм: " << volume << " куб. см.\n";
+    return 0;
+}
