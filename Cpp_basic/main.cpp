58cfe8f8efdfb5994e30c7e083e43a9f7dec4b07
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 16:32:44 +0200

    lab 1 ex 1

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,8 @@
+#include <iostream> // підключення бібліотеки вводу-виводу
 
+int main()
+{
+    std::cout << "  \"Hello, World!\"" << std::endl; // виведення тексту
+    std::cout << "/Bjarne Stroustrup/" << std::endl; // виведення посилання
+    return 0; // повернення нульового значення
+}
