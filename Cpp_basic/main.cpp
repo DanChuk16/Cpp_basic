bbdc5d95c8519213bf356be7cc42b7580f3d9b91
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-20 21:31:16 +0200

    hm 2 ex 2

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int hryvnia, kopiyka;
+    cout << "Enter the amount in hryvnias and kopecks: ";
+    cin >> hryvnia >> kopiyka;
+
+    if (kopiyka > 99) {
+        hryvnia += kopiyka / 100;
+        kopiyka %= 100;
+    }
+
+    hryvnia += kopiyka / 100;
+    kopiyka %= 100;
+
+    cout << "Corrected amount: " << hryvnia << " hryvnias " << kopiyka << " kopecks" << endl;
+
+    return 0;
+}
