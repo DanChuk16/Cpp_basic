c732cb2694f32a1ae04f134af08d35b9976bbbc7
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-23 17:22:55 +0200

    hm 3 ex 3

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int num, max_num;
+    cout << "Enter 7 integers: ";
+    cin >> num;
+    max_num = num;
+
+    for (int i = 1; i < 7; i++)
+    {
+        cin >> num;
+        if (num > max_num)
+        {
+            max_num = num;
+        }
+    }
+
+    cout << "The maximum number is " << max_num << endl;
+
+    return 0;
+}
