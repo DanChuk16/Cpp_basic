067bd8800b79bc0dea3117509b26ef405d2d7583
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-23 16:36:53 +0200

    lab 2 ex 1

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+   int num;
+
+   cout << "Введіть число: ";
+   cin >> num;
+
+   if (num % 2 == 0)
+   {
+      cout << num << " є парним." << endl;
+   } else
+   {
+      cout << num << " не є парним." << endl;
+   }
+
+   return 0;
+}
