7d3e47bbbe31e9963e711f2457ae3752844635e0
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-23 16:52:20 +0200

    lab 2 ex 4

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+   int num1, num2;
+
+   cout << "Введіть перше число: ";
+   cin >> num1;
+
+   cout << "Введіть друге число: ";
+   cin >> num2;
+
+   if (num1 == num2)
+   {
+      cout << "Числа рівні." << endl;
+   }
+   else
+   {
+      cout << "Числа не рівні. Їхні значення за зростанням: ";
+      if (num1 < num2)
+      {
+         cout << num1 << ", " << num2 << endl;
+      }
+      else
+      {
+         cout << num2 << ", " << num1 << endl;
+      }
+   }
+
+   return 0;
+}
