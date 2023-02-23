c2e3a7cef31ff79453ebec398d3add8c4df53af5
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-23 16:41:07 +0200

    lab 2 ex 2

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int num1, num2;
+
+   cout << "Перше число: ";
+   cin >> num1;
+
+   cout << "Друге число: ";
+   cin >> num2;
+
+   if (num1 < num2)
+   {
+      cout << "Менше число: " << num1 << endl;
+   } else {
+      cout << "Менше число: " << num2 << endl;
+   }
+
+   return 0;
+}
 
