3e2e66edf673f465ce9cfecc613b48a923d3c5f1
DanChuk16 <daniilcuhrij@gmail.com>
2023-02-23 16:47:13 +0200

    lab 2 ex 3

--- Cpp_basic/main.cpp
+++ Cpp_basic/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+   int num;
+
+   cout << "Введіть число: ";
+   cin >> num;
+
+   if (num > 0)
+   {
+      cout << num << " є додатнім числом." << endl;
+   } else if (num < 0)
+   {
+      cout << num << " є від'ємним числом." << endl;
+   } else
+   {
+      cout << num << " дорівнює нулю." << endl;
+   }
+
+   return 0;
+}
